e5aca8daa85db4b0c31bd193c8b72368e2a8a4d6
Adam Kobert <adam.kobert@physics.rutgers.edu>
2023-08-23 14:33:13 -0400

    New fits for 2016(APV) and 2017 added. New Binning for 2018 added

--- NanoTool_UL/nuisance/sig25/output/testModel/build.sh
+++
@@ -1,2 +0,0 @@
-combineCards.py ptbin0pass=ptbin0pass.txt ptbin0fail=ptbin0fail.txt ptbin1pass=ptbin1pass.txt ptbin1fail=ptbin1fail.txt ptbin2pass=ptbin2pass.txt ptbin2fail=ptbin2fail.txt ptbin3pass=ptbin3pass.txt ptbin3fail=ptbin3fail.txt ptbin4pass=ptbin4pass.txt ptbin4fail=ptbin4fail.txt ptbin5pass=ptbin5pass.txt ptbin5fail=ptbin5fail.txt ptbin6pass=ptbin6pass.txt ptbin6fail=ptbin6fail.txt ptbin7pass=ptbin7pass.txt ptbin7fail=ptbin7fail.txt ptbin8pass=ptbin8pass.txt ptbin8fail=ptbin8fail.txt > model_combined.txt
-text2workspace.py model_combined.txt
